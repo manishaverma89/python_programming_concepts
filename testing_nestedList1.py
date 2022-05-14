01a39461ce823d3f0f89cd4cb287bd62547adf6d
manisha <manisha.verma23@gmail.com>
2022-05-14 18:47:37 +0530

    testing the concept of nested list

--- testing_nestedList1.py
+++ testing_nestedList1.py
@@ -6,5 +6,5 @@ subList = nestedList[2]
 # access the first element inside the inner list:
 element = nestedList[2][0]
 
-print("List inside the nested list: ", subList)
-print("First element of the sublist: ", element)
+print("List inside the nested list: ", subList)   # gives output: ['a', 1]
+print("First element of the sublist: ", element)  # gives output:  a
