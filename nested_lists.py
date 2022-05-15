0ee1335837312cfca266ec398a4584712cbf7323
manisha <manisha.verma23@gmail.com>
2022-05-15 14:28:37 +0530

    Putting value in the matrix at the position given by the user

--- nested_lists.py
+++ nested_lists.py
@@ -9,7 +9,10 @@ position = input("Where do you want to put the treasure? ")
 column_position = int(position[0])
 row_position = int(position[1])
 
-selected_row = map[row_position-1]
-selected_row[column_position-1] = "X"
+# selected_row = map[row_position-1]
+# selected_row[column_position-1] = "X"
+# or
+
+map[row_position-1][column_position-1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
