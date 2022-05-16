2784dc4fdf1310ea170b988e1f4346a1d1d3028a
manisha <manisha.verma23@gmail.com>
2022-05-16 06:30:51 +0530

    basic example of for loop

---
+++ for_loop.py
@@ -0,0 +1,21 @@
+# for loop
+
+fruits = ["Apple","Orange","Guava","Grapes","Banana"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")  #will be printed after every fruit name
+print("Have these fruits daily") # This line will be printed once after all fruit names
+    
+    
+# output  : 
+"""
+Orange
+Orange pie
+Guava
+Guava pie
+Grapes
+Grapes pie
+Banana
+Banana pie
+Have these fruits daily
+"""
