e0e0395dedad2c36a8419c1e961ce729370b5b25
manisha <manisha.verma23@gmail.com>
2022-06-09 07:06:55 +0530

    sorted list in descending order

--- sorting/sort_desc_order.py
+++ sorting/sort_desc_order.py
@@ -9,3 +9,19 @@ for i in range(0, len(numlist)):
             numlist[j] = temp
             print(numlist)   # It will print All iterations
 print("Elemnts in descending order are: ", numlist) 
+
+
+# output
+'''
+Original list is:  [3, 6, 9, 2, 10]
+[6, 3, 9, 2, 10]
+[9, 3, 6, 2, 10]
+[10, 3, 6, 2, 9]
+[10, 6, 3, 2, 9]
+[10, 9, 3, 2, 6]
+[10, 9, 6, 2, 3]
+[10, 9, 6, 3, 2]
+Elemnts in descending order are:  [10, 9, 6, 3, 2]
+
+
+'''
